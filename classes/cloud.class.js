6021b4462d4ab3bcd7d708c866d96efffca47bd4
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-18 17:27:27 -0400

    draw clouds random added

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,8 +1,10 @@
 class Cloud extends MoveableObject {
+    height = 270;
+    width = 520;
 
     constructor() {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
-    this.x = 200 + Math.random() *500;  // generate Number between 200 up to 700
-    
+    this.x = 10 + Math.random() *600;  // generate Number between 10 up to 800
+this.y = 0 + Math.random() *40;  // generate Number between 0 up to 70
     }
 }
