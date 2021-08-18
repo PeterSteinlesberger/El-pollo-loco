b9ffb13d9b7320a99f50a603a3179a5a3ee338c0
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-18 16:49:47 -0400

    random function added

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -3,6 +3,8 @@ class Chicken extends MoveableObject {
 
 constructor() {
     super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
+this.x = 400;
+
 }
 
 }
