3923b4422e06aca6fdd28e2bb92e8385e95e7557
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-19 15:23:31 -0400

    function addObjectToMap added

--- classes/world.class.js
+++ classes/world.class.js
@@ -4,16 +4,16 @@ class World {
     new Chicken(),
     new Chicken(),
     new Chicken(),
-    ];
-    clouds = [
+];
+clouds = [
         new Cloud()
     ];
 
-    backgroundObjects = [
-new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png')
+backgroundObjects = [
+    new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png')
     ];
 canvas;
-    ctx;
+ctx;
 
 constructor(canvas) {
 this.ctx = canvas.getContext('2d');
@@ -24,13 +24,9 @@ this.draw();
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
        this.addToMap(this.character);
-    this.enemies.forEach(enemy => {
-        this.addToMap(enemy);
-    })
-    this.clouds.forEach(cloud => {
-        this.addToMap(cloud);
-    })
-
+       this.addObjectToMap(this.clouds);
+       this.addObjectToMap(this.enemies);
+       this.addObjectToMap(this.backgroundObjects);
 
     
        //drawCharcter wird immer wieder aufgerufen
@@ -39,6 +35,12 @@ this.draw();
            self.draw();
        })
     }
+
+addObjectToMap(objects) {
+    objects.forEach(object => {
+        this.addToMap(object);
+    })
+}
 
 addToMap(mo) {
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height );
