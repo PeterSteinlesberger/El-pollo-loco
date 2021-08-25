4f4333a3446dd6f5ecc631879fad8122ac957dd4
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-25 17:04:58 -0400

    loadImages function added

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -3,19 +3,28 @@ class MoveableObject {
     y = 210;
     img;
     height = 220;
-    width = 90;
+   width = 90;
+imageCache = {};
 
 loadImage(path) {
 this.img = new Image();
 this.img.src = path;
 }
 
+loadImages(arr) {
+    arr.forEach((path) => {
+ let img = new Image();
+img.src = path;
+this.imageCache[path] = path;   
+    });
+}
 
     moveRight() {
         console.log('Moving Right', character);
     }
 
     moveLeft() {
-
     }
+
+
 }
