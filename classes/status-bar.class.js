2c1b7e710941ca7145abc104210af2af2d44b9bb
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-30 19:02:25 -0400

    resolve function added

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -1,6 +1,6 @@
 class StatusBar {
 
-IMAGE_ENERGY = [
+IMAGES_ENERGY = [
 'img/7.Marcadores/Barra/Marcador vida/azul/0_.png',
 'img/7.Marcadores/Barra/Marcador vida/azul/0_.png',
 'img/7.Marcadores/Barra/Marcador vida/azul/0_.png',
@@ -9,5 +9,34 @@ IMAGE_ENERGY = [
 'img/7.Marcadores/Barra/Marcador vida/azul/0_.png'
 ];
 
+percentage = 100;
+
+
+constructor() {
+this.loadImages(this.IMAGES_ENERGY);
+
+}
+
+
+setPercentage(percentage) {
+    this.percentage = percentage;
+}
+    rexolveImageIndex() {
+if(this.percentage == 100) {
+return 5;
+} else if(this.percentage > 80) {
+    return 4;
+} else if(this.percentage > 60) {
+    return 3;
+} else if(this.percentage > 40) {
+    return 2;
+} else if(this.percentage > 20) {
+    return 1;
+} else if(this.percenage < 20) {
+    return 0;
+}
+
+
+}
 
 }
