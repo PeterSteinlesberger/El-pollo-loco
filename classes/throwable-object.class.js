6a2c4dee7c83cc58fbaafa3aabb24cecfa35ca35
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-31 17:01:52 -0400

    throw Function added

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,19 +1,26 @@
 class ThrowableObject extends MoveableObject {
 
-    speedY = 30;
-    speedX = 24;
+
 
 
     constructor() {
         super().loadImage('img/7.Marcadores/Icono/Botella.png');
 this.x = 100;
 this.y = 100;
-
+this.height = 60;
+this.width = 50;
+this.throw(100, 170);
     }
 
-throw() {
+throw(x, y) {
 
-
+    this.x = x;
+    this.y = y;
+    this.speedY = 30;
+    this.applyGravity();
+    setInterval(() => {
+this.x += 10;
+    }, 20);
 }
 
 
