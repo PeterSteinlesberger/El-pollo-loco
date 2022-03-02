a594fceddd57b9c3318f5177f8133ebfa447edd0
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-03-02 17:15:34 +0100

    add: throwBottle animation

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -4,29 +4,31 @@ class ThrowableObject extends MoveableObject {
 
 
     constructor(x, y) {
-        super().loadImage('img/6.botella/Rotación/Mesa de trabajo 1 copia 3.png');
-this.x = x;
-this.y = y;
-this.height = 60;
-this.width = 50;
-this.throw();
+        super();
+        this.loadImages(this.IMAGES_SHOOT_BOTTLE);
+        this.x = x;
+        this.y = y;
+        this.height = 60;
+        this.width = 50;
+        this.throw();
     }
 
     IMAGES_SHOOT_BOTTLE = [
-        'img/6.botella/Rotación/Mesa de trabajo 1 copia 3.png',  
-        'img/6.botella/Rotación/Mesa de trabajo 1 copia 4.png', 
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 3.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 4.png',
         'img/6.botella/Rotación/Mesa de trabajo 1 copia 5.png',
-        'img/6.botella/Rotación/Mesa de trabajo 1 copia 6.png' 
-      ];
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 6.png'
+    ];
 
 
-throw() {
-    this.speedY = 26;
-    this.applyGravity();
-    setInterval(() => {
-this.x += 10;
-    }, 20);
-}
+    throw() {
+        this.speedY = 26;
+        this.applyGravity();
+        this.animateImages(this.IMAGES_SHOOT_BOTTLE);
+        setInterval(() => {
+            this.x += 10;
+        }, 20);
+    }
 
 
 
